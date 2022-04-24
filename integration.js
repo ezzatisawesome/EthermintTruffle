dc0b6caf69475955402fd2f64f2c58e023af4f6a
ezzatisawesome <ezzatisawesome@gmail.com>
2022-04-24 05:36:31 -0500

    asdasdfasdf

--- integration.js
+++ integration.js
@@ -2,58 +2,65 @@
  * // Setup metamask wallet with some fake tokens
  * // Attach to web3.js
  * TODO Bring assets from eth to evmos
- * TODO Bring assets from evmos to cosmos
+ * TODO Bring assets from evmos to wasm
  * TODO Setup eth signer (alchemy web3?)
  * TODO setup cosmos signer with cosmjs
  * TODO Setup local evm node (geth?)
  */
 
-/**
- * ethJsonRpc: http 8545 --> 8545
- * 
- * -----
- * evmosGRpc: http 9090 --> 9092
- * evmosRpc: tcp 26657 --> 26659
- * evmosJsonRpc: http 8545 --> 8847
- * -----
- * cosmosGRpc: http 9090 --> 9094
- * cosmosRpc: tcp 26657 --> 26661
- * cosmosRest: http 1317 --> 1321
- */
+import { pebblenetOptions } from "./walletHelper.js"
 
 import Web3 from "web3"
+import fetch from "node-fetch"
+
 import { ethToEvmos, evmosToEth } from "@tharsis/address-converter"
+import { generateEndpointBroadcast, generatePostBodyBroadcast, generateEndpointAccount } from '@tharsis/provider'
+import { createTxRawEIP712, signatureToWeb3Extension, createMessageSend } from '@tharsis/transactions'
+
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate"
 import { QueryClient, setupBankExtension,  } from "@cosmjs/stargate"
 import { Tendermint34Client } from "@cosmjs/tendermint-rpc"
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing"
 
-
+import { signTypedData, SignTypedDataVersion } from "@metamask/eth-sig-util"
+=
 /* CLIENTS */
 // ETH
-const evmosJsonRpcUrl = "http://localhost:8545"
+const evmosJsonRpcUrl = "http://0.0.0.0:8545"
 const web3 = new Web3(evmosJsonRpcUrl)
 // EVMOS
-const evmosRpcUrl = "tcp://0.0.0.0:26660"
+const evmosHttpUrl = "http://0.0.0.0:1317"
+const evmosRpcUrl = "tcp://0.0.0.0:26657"
 const evmosTmClient = await Tendermint34Client.connect(evmosRpcUrl)
 const evmosQueryClient = QueryClient.withExtensions(evmosTmClient, setupBankExtension)
-// COSMOS
-// const cosmosRpcUrl = ""
-// const cosmosTmClient = await Tendermint34Client.connect(cosmosRpcUrl)
-// const cosmotQueryClient = QueryClient.withExtensions(cosmosTmClient, setupBankExtension)
+// WASM
+const wasmRpcUrl = pebblenetOptions.httpUrl
+const wasmTmClient = await Tendermint34Client.connect(wasmRpcUrl)
+const wasmQueryClient = QueryClient.withExtensions(wasmTmClient, setupBankExtension)
+
 
 /* TEST WALLETS + ACCOUNTS */
+const testMnemonic = "hand flavor weasel connect valley debris like useful exile machine faculty join eager catch hospital banner bus enjoy course print kitten poverty inner section" // ! will need to be stored in some db
 // ETH
-const ethTestAddress = "0xd04DD171C053c37dCbf3ca7E7E03caD9E87198E0"
-const ethTestPrivKey = "fe7992530904e3c74374aa4fe602c6c0d6d7267266819de2f5547ee1e7ee8bbb"
+const ethTestAddress = "0xAf61b4b63B8B8406bC0C8186299b46ea166a44E0"
+const ethTestPrivKey = "a1bf8edafe42bcac098d411e91d2e455a6f60a1217a5db27cf91de46b925b118"
 const ethTestWallet = web3.eth.accounts.wallet.add(ethTestPrivKey)
 // EVMOS
 const evmosTestAddress = ethToEvmos(ethTestAddress)
-// COSMOS
-const cosmosTestMnemonic = "dog praise gorilla actor idea aerobic balcony shoe help syrup used viable plastic riot river cricket crawl dune outside lava cinnamon bounce coconut lobster" // ! will need to be stored in some db
-const cosmosTestWallet = await DirectSecp256k1HdWallet.fromMnemonic(cosmosTestMnemonic)
-const [cosmosTestAccount] = await cosmosTestWallet.getAccounts()
-const cosmosTestAddress = cosmosTestAccount.address
+const options = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+}
+await (await fetch(`${evmosHttpUrl}${generateEndpointAccount(evmosTestAddress)}`, options)).json()
+.then((account => {
+    console.log(account.account.base_account)
+}))
+// WASM
+const wasmTestWallet = await DirectSecp256k1HdWallet.fromMnemonic(testMnemonic, { hdPaths: [pebblenetOptions.hdPath], prefix: pebblenetOptions.bech32prefix })
+const [wasmTestAccount] = await wasmTestWallet.getAccounts()
+const wasmTestAddress = wasmTestAccount.address
+wasmTestAccount.pubkey
+const wasmClient = await SigningCosmWasmClient.connectWithSigner(wasmRpcUrl, wasmTestWallet);
 
 
 /* MAIN WALLETS + ACCOUNTS */
@@ -62,16 +69,57 @@ const ethMainAddress = "0xDCFFe3264d4219b1C0DC39EDe7fc2af93B5E01F8"
 // EVMOS
 const evmosMainAddress = ethToEvmos(ethMainAddress)
 
-const options = {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-}
 
 console.log(`ethTestAddress: ${ethTestAddress}`)
 console.log(`evmosTestAddress: ${evmosTestAddress}`)
-console.log(`cosmosTestAddress: ${cosmosTestAddress}`)
-
+console.log(`wasmTestAddress: ${wasmTestAddress}`)
 console.log(await evmosQueryClient.bank.allBalances(evmosTestAddress))
 console.log(await evmosQueryClient.bank.allBalances(evmosMainAddress))
-console.log(`Transaction Count - Test Account: ${await web3.eth.getTransactionCount(ethTestAddress)}`) // ! gets info from evmos node --> look at RPC url
+console.log(await wasmQueryClient.bank.allBalances(wasmTestAddress))
+console.log(`Transaction Count - Test Account: ${await web3.eth.getTransactionCount(ethTestAddress)}`)
 console.log(`Transaction Count - Main Account: ${await web3.eth.getTransactionCount(ethMainAddress)}`)
+
+
+const evmosTransaction = async () => {
+const chain = {
+    chainId: 9000,
+    cosmosChainId: 'evmos_9000-1',
+}
+const sender = {
+    accountAddress: evmosTestAddress,
+    sequence: 7,
+    accountNumber: 0,
+    pubkey: 'A9qXeXtq2mElT9aGEeUX/eJWzgnP58wdV/70MiWS2/G8', // // get public key
+    privKey: Buffer.from('a1bf8edafe42bcac098d411e91d2e455a6f60a1217a5db27cf91de46b925b118', 'hex') // ! get priv key
+}
+const fee = {
+    amount: '20',
+    denom: 'aevmos',
+    gas: '200000',
+}
+const memo = ''
+const params = {
+    destinationAddress: evmosMainAddress,
+    amount: '1500000000000000000000000',
+    denom: 'aevmos',
+}
+    const msg = createMessageSend(chain, sender, fee, memo, params)
+    console.log(SignTypedDataVersion.V4)
+    let signature = signTypedData({privateKey: sender.privKey, data: msg.eipToSign, version: SignTypedDataVersion.V4})
+    let extension = signatureToWeb3Extension(chain, sender, signature)
+
+    let rawTx = createTxRawEIP712(msg.legacyAmino.body, msg.legacyAmino.authInfo, extension)
+    const postOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: generatePostBodyBroadcast(rawTx),
+    }
+    let broadcastPost = await fetch(
+        `${evmosHttpUrl}${generateEndpointBroadcast()}`,
+        postOptions
+    )
+    let response = await broadcastPost.json();
+    console.log(response)
+}
+// evmosTransaction()
+
--- test.js
+++ test.js
@@ -10,7 +10,7 @@ const evmosAddress = "evmos1l0f7llfvjmqy5k65x4ycq8vqpmchszw8c84uk7"
 // const web3 = new Web3(evmosJsonRpcUrl)
 // console.log(`Transaction Count - Test Account: ${await web3.eth.getBalance(ethAddress)}`) // ! gets info from evmos node --> look at RPC url
 
-const rpcUrl = "tcp://0.0.0.0:26657"
+const rpcUrl = "http://0.0.0.0:26657"
 const tmClient = await Tendermint34Client.connect(rpcUrl)
 const queryClient = QueryClient.withExtensions(tmClient, setupBankExtension)
 console.log(await queryClient.bank.allBalances(evmosAddress))
